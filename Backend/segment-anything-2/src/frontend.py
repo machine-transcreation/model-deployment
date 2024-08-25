dfbb3800f171ca82d94184545849c97a524bbd94
Jainish Patel <jaicode2020@gmail.com>
2024-08-25 14:24:53 -0400

    added sam2 point inputs

--- Backend/segment-anything-2/src/frontend.py
+++ Backend/segment-anything-2/src/frontend.py
@@ -1,14 +1,28 @@
+import numpy as np
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 from PIL import Image
+import torch
+from sam2.build_sam import build_sam2
+from sam2.sam2_image_predictor import SAM2ImagePredictor
+
+@st.cache_resource
+def build_predictor():
+    checkpoint = "./checkpoints/sam2_hiera_large.pt"
+    model_cfg = "sam2_hiera_l.yaml"
+    predictor = SAM2ImagePredictor(build_sam2(model_cfg, checkpoint))
+
+    return predictor
+
+predictor = build_predictor()
 
 def main():
-    st.title("Image Plotter: Green + and Red - Points")
+    st.title("SAM2: Point-Based")
 
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        image = Image.open(uploaded_file)
+        image = Image.open(uploaded_file).resize((512, 512))
         
         width, height = image.size
         
@@ -53,8 +67,21 @@ def main():
             st.session_state.red_points = current_red_points
 
         if st.button("Create Mask"):
-            st.write("Green + Points:", st.session_state.green_points)
-            st.write("Red - Points:", st.session_state.red_points)
+            sam_points = []
+            sam_labels = []
+
+            for point in st.session_state.green_points:
+                sam_points.append(point)
+                sam_labels.append(1)
+
+            for point in st.session_state.red_points:
+                sam_points.append(point)
+                sam_labels.append(0)
+
+            with torch.inference_mode(), torch.autocast("cuda", dtype=torch.bfloat16):
+                predictor.set_image(np.asarray(image))
+                masks, _, _ = predictor.predict(point_coords = np.array(sam_points), point_labels = np.array(sam_labels), multimask_output = False)
+                print(masks)
 
 if __name__ == "__main__":
     main()
