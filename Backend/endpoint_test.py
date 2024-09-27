94db9d49e3c2123bf8db0c36268f32c7059a8081
Jainish Patel <jaicode2020@gmail.com>
2024-09-27 17:36:57 -0400

    controlnet docker

--- Backend/endpoint_test.py
+++ Backend/endpoint_test.py
@@ -4,13 +4,13 @@ import requests
 
 load_dotenv(".env")
 
-url = os.getenv("ANYDOOR_ENDPOINT")
+url = os.getenv("POWERPAINT_ENDPOINT")
 headers = {
     'Authorization': f'Bearer {os.getenv("RUNPOD_KEY")}',
     'Content-Type': 'application/json'
 }
 
-with open('testing/anydoor.json', 'r') as file:
+with open('testing/powerpaint.json', 'r') as file:
     payload = file.read()
 
 response = requests.post(url, headers=headers, data=payload)
